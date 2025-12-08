876e4bd2c1422e685128263fcf8a510af2408443
ykaurarshpreet <ykaur.arshpreet@gmail.com>
2025-12-08 01:32:26 -0800

    added new token

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -4,6 +4,6 @@ import axios from 'axios'
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
-        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzY3OTZiMzY0NzU0YmQxM2NlNmY3ZiIsInVzZXJuYW1lIjoidXNlcjIiLCJlbWFpbCI6InVzZXIyQHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY1MTc4OTI1LCJleHAiOjE3NjUxODYxMjV9.FHknNsw9QIt0fT04ful2nI8gvDCCN_rK4YQ1D23FiBw"
+        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzY5YWVhZTU2ZmIxMjllMTQwOTg5YyIsInVzZXJuYW1lIjoidXNlcjQiLCJlbWFpbCI6InVzZXI0QHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY1MTg2MjkyLCJleHAiOjE3NjUyNzI2OTJ9.JEHmIuHk4ctES2hX8kOgQncnYKTykoVG5PywQAFdyJ0"
     }
 });
