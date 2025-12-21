cd6f86468fc29ccdbec6893d41ca8a1d01177c78
ykaurarshpreet <ykaur.arshpreet@gmail.com>
2025-12-21 01:44:31 -0800

    updated

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -4,6 +4,6 @@ import axios from 'axios'
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
-        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzdiNTFjODdkZTY5M2NmMTUzZDBlNyIsInVzZXJuYW1lIjoidXNlcjciLCJlbWFpbCI6InVzZXI3QHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY1NDczNDc0LCJleHAiOjE3NjU1NTk4NzR9.BZrKp4VFg_MHlMlewCqx5hnvFt7xoGloc5wifRPJpRQ"
+        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5NDdiZDA5YzYyMzIyNzk2YjllN2YyZSIsInVzZXJuYW1lIjoidXNlcjEwMCIsImVtYWlsIjoidXNlcjEwMEB0ZXN0LmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NjMwOTE1MSwiZXhwIjoxNzY2Mzk1NTUxfQ.A0VP0m8jL7DmpMJtf8Biiri2noMEEtBLH6IGqpnXiOU"
     }
 });
